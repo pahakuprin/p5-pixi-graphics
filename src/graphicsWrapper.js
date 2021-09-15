fb88428d8806c7b2884bcecda281c888364240c5
Павел <pavel@iMac-Tatana.local>
2021-09-15 12:14:49 +0300

    feat: add animation loop

--- src/graphicsWrapper.js
+++ src/graphicsWrapper.js
@@ -1,21 +1,30 @@
+import { Ticker } from 'pixi.js';
 import { SmoothGraphics } from '@pixi/graphics-smooth';
 
-export const createGraphics = (draw, ...args) => {
+export const createGraphics = (draw, state, ...args) => {
   const drawingContext = new SmoothGraphics(...args);
 
   let fillColor = 0xffffff;
 
   const p5 = {
     drawingContext,
-    fill: (color) => (fillColor = parseInt(color.slice(1), 16)),
+    clear: () => drawingContext.clear(),
+    fill: (color) => (fillColor = parseColor(color)),
     rect: (x, y, w, h = w, c = 0) => {
       drawingContext.beginFill(fillColor);
       drawingContext.drawRoundedRect(x, y, w, h, c);
       drawingContext.endFill();
     },
+    redraw: (update) => {
+      Object.assign(state, update(state));
+      window.requestAnimationFrame(() => draw(p5, state));
+      // Ticker.shared.addOnce(() => draw(p5, state));  // gets in the infinity loop
+    },
   };
 
-  draw(p5);
+  draw(p5, state);
 
   return drawingContext;
 };
+
+export const parseColor = (color) => parseInt(color.slice(1), 16);
--- src/index.js
+++ src/index.js
@@ -8,13 +8,51 @@ const app = new Application({
   backgroundColor: 0xdcdcdc,
   resizeTo: window,
 });
+
 document.body.appendChild(app.view);
 
-const graphics = createGraphics((p5) => {
+// Static drawing example
+
+const staticGraphics = createGraphics((p5) => {
   p5.fill('#008C6E');
-  p5.rect(50, 50, 200, 200, 100);
-  p5.fill('#DC872D');
-  p5.rect(300, 300, 100);
+  p5.rect(50, 50, 200, 200, 50);
 });
 
-app.stage.addChild(graphics);
+// Simple animation loop example with one holded value – time
+
+const pulseGraphics = createGraphics(
+  (p5, { time }) => {
+    const width = Math.sin(time) * 300;
+
+    p5.clear();
+    p5.fill('#DC872D');
+    p5.rect(300, 300, width);
+
+    // Pass callback function to redraw that calculate new time value
+    p5.redraw(() => ({ time: time + 0.01 }));
+  },
+  { time: 0 }
+); // 0 is initial value of time
+
+// Full drawing loop example with condition exit
+
+const initialState = { rectWidth: 100, rectHeight: 0 };
+
+const update = ({ rectHeight }) => ({
+  rectHeight: rectHeight + 2,
+});
+
+const render = (p5, state) => {
+  const { rectWidth, rectHeight } = state;
+
+  p5.fill('#3294C7');
+  p5.rect(700, 200, rectWidth, rectHeight);
+
+  if (rectHeight < 300) {
+    p5.redraw(update);
+  }
+};
+
+const conditionGraphics = createGraphics(render, initialState);
+
+app.stage.addChild(staticGraphics, pulseGraphics, conditionGraphics);
